5b0678da6b86cc2f864dcaf0d6a1e66e283da49a
Anup Kumar <anup.rulez@gmail.com>
2023-04-04 15:11:30 +0200

    update

--- tools/prot_pred/main.py
+++ tools/prot_pred/main.py
@@ -7,9 +7,9 @@ import argparse
 import time
 
 import torch
-import h5py
 from transformers import T5Tokenizer, T5Model, T5EncoderModel
 from Bio import SeqIO
+import h5py
 
 
 def process_sequences(seq_file):
@@ -32,31 +32,31 @@ def encode_seqs(seqs_fasta, out_file):
     model = model.to(device)
     model = model.eval()
     print("tokenization...")
-	ids = tokenizer.batch_encode_plus(
-		sequences, add_special_tokens=True, padding=True
-	)
-	input_ids = torch.tensor(ids["input_ids"]).to(device)
-	attention_mask = torch.tensor(ids["attention_mask"]).to(device)
+    ids = tokenizer.batch_encode_plus(
+        sequences, add_special_tokens=True, padding=True
+    )
+    input_ids = torch.tensor(ids["input_ids"]).to(device)
+    attention_mask = torch.tensor(ids["attention_mask"]).to(device)
 	
-	print("computing embedding...")
+    print("computing embedding...")
 
-	agg_embedding = list()
-	s_time = time.time()
+    agg_embedding = list()
+    s_time = time.time()
 	
-	batch_size = 1
+    batch_size = 1
 
-	for i in range(int(len(sequences) / batch_size)):
-		curr = i * batch_size
-		nex = i * batch_size + batch_size
-		with torch.no_grad():
-		    embedding = model(input_ids=input_ids[curr:nex,], attention_mask=attention_mask[curr:nex,])
-		embedding = embedding.last_hidden_state.cpu().numpy()
-		agg_embedding.extend(embedding)
-	e_time = time.time()
-	print("embedding computed")
+    for i in range(int(len(sequences) / batch_size)):
+        curr = i * batch_size
+        nex = i * batch_size + batch_size
+        with torch.no_grad():
+            embedding = model(input_ids=input_ids[curr:nex,], attention_mask=attention_mask[curr:nex,])
+            embedding = embedding.last_hidden_state.cpu().numpy()
+            agg_embedding.extend(embedding)
+    e_time = time.time()
+    print("embedding computed")
 	
-	#hf = h5py.File('data/save_agg_embedding_{}.h5'.format(str(len(sequences))), 'w')
-	hf = h5py.File(out_file, 'w')
+    #hf = h5py.File('data/save_agg_embedding_{}.h5'.format(str(len(sequences))), 'w')
+    hf = h5py.File(out_file, 'w')
     hf.create_dataset('embedding', data=agg_embedding)
     hf.close()
 
@@ -72,6 +72,9 @@ if __name__ == "__main__":
     args = vars(arg_parser.parse_args())
     fasta_file = args["fasta_file"]
     out_file = args["output_embed"]
+    
+    print(torch.__version__)
+    print(fasta_file, out_file)
 
     fasta_seqs = process_sequences(fasta_file)
 
